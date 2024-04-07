61c6d01caf8f9bf2c2c3d95e56ffa9301f14b9d8
1Edum <eduardomatosdesouza2@gmail.com>
2024-04-07 16:11:40 -0300

    Layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
  
 
 export default function Home() {
-  return (
-    <main>
-
-    </main>
+  return ( 
+    <div className="w-screen h-screen">
+      <div className="flex">
+        <aside className="bg-red-400 w-1/5 h-[90vh]">
+            fefefefe
+        </aside>
+        <main className="flex-1 bg-red-300 w-4/5 h-[90vh]">
+          fefef
+        </main>
+      </div>
+      <footer className="">
+          fefe
+      </footer>
+    </div>
   );
 }
