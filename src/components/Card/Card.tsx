04401c0fed4411466d71d4aa8b5d48df5f0b394c
1Edum <eduardomatosdesouza2@gmail.com>
2024-04-16 20:11:43 -0300

    Main

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,16 +1,15 @@
-import React from 'react'
+import React from "react";
 
 interface CardProps {
-    title: string;
-  }
+  title: string;
+}
 
-  const Card: React.FC<CardProps> = ({ title }) => {
-    return (
-      <div>
-        <h1>{title}</h1>
-        
-      </div>
-    );
-  }
-  
-  export default Card;
+const Card: React.FC<CardProps> = ({ title }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/Main/Favorites.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { musicas } from "@/Dados/Musicas";
+
+export default function Favorites() {
+  const idsSelecionados = [1, 7, 8, 4, 5, 9, 10, 11];
+  const musicasSelecionadas = musicas.filter((musica) =>
+    idsSelecionados.includes(musica.id)
+  );
+
+  return (
+    <section className="flex flex-col">
+      <div className="grid grid-rows-2 grid-flow-col gap-y-2 gap-x-4">
+        {musicasSelecionadas.map((musica) => (
+          <div key={musica.id} className="bg-white px-2 py-1 w-full rounded-sm">
+            <h4>{musica.titulo}</h4>
+            <p className="text-xs">{musica.artista}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
