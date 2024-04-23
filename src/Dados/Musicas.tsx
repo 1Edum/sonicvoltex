e5ef348c4b767d8956d87a2511937246781ae756
1Edum <eduardomatosdesouza2@gmail.com>
2024-04-23 18:52:41 -0300

    Atualização Banco de Dados

--- src/Dados/Musicas.tsx
+++ src/Dados/Musicas.tsx
@@ -5,150 +5,99 @@ export const musicas = [
         id: 1,
         titulo: "Evidências",
         artista: "Chitãozinho & Xororó",
-        genero: "Sertanejo"
+        genero: "Sertanejo",
+        img: "https://firebasestorage.googleapis.com/v0/b/sonicvoltex.appspot.com/o/1.png?alt=media&token=4df34e1d-2133-4eac-8163-cab124a4d89d"
     },
     {
         id: 2,
         titulo: "Ai Se Eu Te Pego",
         artista: "Michel Teló",
-        genero: "Sertanejo"
+        genero: "Sertanejo",
+        img: "https://firebasestorage.googleapis.com/v0/b/sonicvoltex.appspot.com/o/2.png?alt=media&token=dfd9392b-9bd7-4971-9052-a25d0952f70e"
     },
     {
         id: 3,
         titulo: "Amor de Sobremesa",
         artista: "Jorge & Mateus",
-        genero: "Sertanejo"
+        genero: "Sertanejo",
+        img:"https://firebasestorage.googleapis.com/v0/b/sonicvoltex.appspot.com/o/3.png?alt=media&token=02cd9e30-fdff-46cb-bffd-a92f1c0e4ddc"
     },
     {
         id: 4,
         titulo: "Chegou o Verão",
         artista: "Sorriso Maroto",
-        genero: "Pagode"
+        genero: "Pagode",
+        img:"https://firebasestorage.googleapis.com/v0/b/sonicvoltex.appspot.com/o/4.png?alt=media&token=b65647dd-fb9d-4b26-bdff-727eca976fa7"
     },
     {
         id: 5,
         titulo: "Festa",
         artista: "Ivete Sangalo",
-        genero: "Pagode"
+        genero: "Pagode",
+        img:"https://firebasestorage.googleapis.com/v0/b/sonicvoltex.appspot.com/o/5.png?alt=media&token=0dd82180-d558-402b-9225-dc48407c8658"
     },
     {
         id: 6,
         titulo: "Beijo Bom",
         artista: "Matheus & Kauan",
-        genero: "Sertanejo"
+        genero: "Sertanejo",
+        img:"https://firebasestorage.googleapis.com/v0/b/sonicvoltex.appspot.com/o/6.png?alt=media&token=df044a15-a39b-49e7-ba72-c5b90270d7e0"
     },
     {
         id: 7,
         titulo: "Isso Cê Num Conta",
         artista: "Bruno & Marrone",
-        genero: "Sertanejo"
+        genero: "Sertanejo",
+        img:"https://firebasestorage.googleapis.com/v0/b/sonicvoltex.appspot.com/o/7.png?alt=media&token=a5004b37-b2f0-4e3c-b12e-ebd6a849b3f7"
     },
     {
         id: 8,
         titulo: "Beijo de Varanda",
         artista: "Bruno & Marrone",
-        genero: "Sertanejo"
+        genero: "Sertanejo",
+        img:"https://firebasestorage.googleapis.com/v0/b/sonicvoltex.appspot.com/o/8.png?alt=media&token=0bfb3d9e-20be-4484-9403-cb73f05f49ab"
     },
     {
         id: 9,
         titulo: "Ela Só Quer Paz",
         artista: "Projota",
-        genero: "Pagode"
+        genero: "Pagode",
+        img:"https://firebasestorage.googleapis.com/v0/b/sonicvoltex.appspot.com/o/9.png?alt=media&token=32f89a56-8b31-445c-9c49-89915755acb1"
     },
     {
         id: 10,
         titulo: "Ciumeira",
         artista: "Marília Mendonça",
-        genero: "Sertanejo"
+        genero: "Sertanejo",
+        img:"https://firebasestorage.googleapis.com/v0/b/sonicvoltex.appspot.com/o/10.png?alt=media&token=c6bb0744-6cda-4436-ae4d-126f66389fd5"
     },
     {
         id: 11,
         titulo: "Flor",
         artista: "Jorge & Mateus",
-        genero: "Sertanejo"
+        genero: "Sertanejo",
+        img:"https://firebasestorage.googleapis.com/v0/b/sonicvoltex.appspot.com/o/11.png?alt=media&token=3786f976-8db0-4eb9-8a7f-839b732c7a3a"
     },
     {
         id: 12,
         titulo: "Baldinho de Gelo",
         artista: "Luan Santana",
-        genero: "Sertanejo"
+        genero: "Sertanejo",
+        img:"https://firebasestorage.googleapis.com/v0/b/sonicvoltex.appspot.com/o/12.png?alt=media&token=23a6c6f7-8339-477a-b37b-a50d302634aa"
     },
     {
         id: 13,
         titulo: "Medo Bobo",
         artista: "Maiara & Maraisa",
-        genero: "Sertanejo"
+        genero: "Sertanejo",
+        img:"https://firebasestorage.googleapis.com/v0/b/sonicvoltex.appspot.com/o/13.png?alt=media&token=75226f31-e9ba-44ff-b82b-c9a497f100f3"
     },
     {
         id: 14,
         titulo: "A Culpa é Sua",
         artista: "Marília Mendonça",
-        genero: "Sertanejo"
+        genero: "Sertanejo",
+        img:"https://firebasestorage.googleapis.com/v0/b/sonicvoltex.appspot.com/o/14.png?alt=media&token=8b7345f4-857e-4851-9921-0dfc9adb9ad3"
     },
-    {
-        id: 15,
-        titulo: "Deixa Acontecer",
-        artista: "Grupo Revelação",
-        genero: "Pagode"
-    },
-    {
-        id: 16,
-        titulo: "Eu, Você, o Mar e Ela",
-        artista: "Luan Santana",
-        genero: "Sertanejo"
-    },
-    {
-        id: 17,
-        titulo: "Sinal Disfarçado",
-        artista: "Zé Neto & Cristiano",
-        genero: "Sertanejo"
-    },
-    {
-        id: 18,
-        titulo: "Flores em Vida",
-        artista: "Zezé Di Camargo & Luciano",
-        genero: "Sertanejo"
-    },
-    {
-        id: 19,
-        titulo: "Lindo Balão Azul",
-        artista: "Turma do Pagode",
-        genero: "Pagode"
-    },
-    {
-        id: 20,
-        titulo: "Dona Maria",
-        artista: "Thiago Brava ft. Jorge",
-        genero: "Sertanejo"
-    },
-    {
-        id: 21,
-        titulo: "Se Eu Pedir Cê Volta",
-        artista: "Henrique & Juliano",
-        genero: "Sertanejo"
-    },
-    {
-        id: 22,
-        titulo: "Sogrão Caprichou",
-        artista: "Luan Santana",
-        genero: "Sertanejo"
-    },
-    {
-        id: 23,
-        titulo: "10%",
-        artista: "Maiara & Maraisa",
-        genero: "Sertanejo"
-    },
-    {
-        id: 24,
-        titulo: "Cerveja de Garrafa",
-        artista: "Atitude 67",
-        genero: "Pagode"
-    },
-    {
-        id: 25,
-        titulo: "Nosso Contrato",
-        artista: "Jorge & Mateus",
-        genero: "Sertanejo"
-    },
+    
 ];
