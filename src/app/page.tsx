c2025371174cacd751a3bdd8ab382e20fca7bbb3
1Edum <eduardomatosdesouza2@gmail.com>
2024-08-21 20:18:56 -0300

    feat: create dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,9 @@ interface PageProps {
 }
 
 const Home: React.FC<PageProps> = () => {
-  const [state, setState] = useState("Initial State");
+  const [state, setState] = useState('light');
   return (
-    <main className="flex-1  md:w-4/5 md:h-[90vh] w-screen bg-gradient-to-tr from-zinc-100 from-40% to-green-400">
+    <main className={"flex-1  md:w-4/5 md:h-[90vh] w-screen bg-gradient-to-tr " + `${state === 'dark' ? 'from-zinc-900' : 'from-zinc-100'} from-40% to-green-400`}>
       <SectionTop state={state} setState={setState} />
       <section className="overflow-y-auto h-[600px]">
         <Favorites />
