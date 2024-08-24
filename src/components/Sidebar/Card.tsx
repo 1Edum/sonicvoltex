4f6f157d47c455f4890bf4c556757bf1addab32c
1Edum <eduardomatosdesouza2@gmail.com>
2024-08-24 11:16:24 -0300

    feat: add mode dark in card component

--- src/components/Sidebar/Card.tsx
+++ src/components/Sidebar/Card.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React from "react";
+
+import { Trash2, Pencil } from "lucide-react";
 
 interface CardProps {
   nome: string;
@@ -8,24 +10,19 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ nome, onEditar, onExcluir }) => {
   return (
-    <div className="w-full flex justify-between items-center hover:bg-zinc-300 rounded-md p-2 ">
-      <div className='flex items-center'>
-        <div className='bg-green-400 rounded md:w-10 md:h-10 w-5 h-5  mr-3'>
-        </div>
-        <p className='group-hover:text-zinc-500'>{nome}</p>
+    <div className="w-full flex justify-between items-center hover:bg-zinc-300 rounded-md p-2 dark:hover:bg-zinc-800">
+      <div className="flex items-center">
+        <div className="bg-green-400 rounded md:w-10 md:h-10 w-5 h-5 mr-3"></div>
+        <p className="text-sm text-sidebar ">{nome}</p>
       </div>
-        <div className='flex items-center gap-2'> 
-            <button onClick={onEditar}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="md:w-6 md:h-6 w-4 h-4">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
-                </svg>
-            </button>
-            <button onClick={onExcluir}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="md:w-6 md:h-6 w-4 h-4">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
-                </svg>
-            </button>
-        </div>
+      <div className="flex items-center gap-2">
+        <button onClick={onEditar}>
+          <Pencil className="icon-sidebar"/>
+        </button>
+        <button onClick={onExcluir}>
+          <Trash2 className="icon-sidebar"/>
+        </button>
+      </div>
     </div>
   );
 };
