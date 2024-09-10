26ec6d6a0ccc3c216b67aa5f456291b9ca0bf530
1Edum <eduardomatosdesouza2@gmail.com>
2024-09-10 18:48:08 -0300

    feat: updating formulario component with styles and interface props

--- src/components/Sidebar/Formulario.tsx
+++ src/components/Sidebar/Formulario.tsx
@@ -1,29 +1,41 @@
-import React, { useState } from 'react';
+
+
+import { useState } from "react";
+
+export interface CardData {
+  id: number;
+  nome: string;
+
+}
 
 interface FormularioProps {
+  card: CardData | null;
   onAdicionar: (nome: string) => void;
 }
 
-const Formulario: React.FC<FormularioProps> = ({ onAdicionar }) => {
-  const [nome, setNome] = useState('');
+const Formulario: React.FC<FormularioProps> = ({ card, onAdicionar }) => {
+  const [nome, setNome] = useState(card ? card.nome : ""); // Preenche o nome se estiver editando um cartão
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (!nome.trim()) return;
-    onAdicionar(nome);
-    setNome('');
+    if (nome.trim()) {
+      onAdicionar(nome); // Chama a função de adição ou edição
+      setNome("");
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit} className='flex items-center justify-between gap-4'>
+    <form onSubmit={handleSubmit} className="flex flex-col gap-2 ">
       <input
         type="text"
-        className='border-2 px-2 py-1 focus:outline-none'
-        placeholder="Nome da Playlist"
         value={nome}
         onChange={(e) => setNome(e.target.value)}
+        placeholder="Nome da playlist"
+        className="border-2 border-green-400 rounded-md p-2 dark:bg-black text-sidebar"
       />
-      <button type="submit" className='bg-green-400 text-white px-2 py-1 rounded-md hover:bg-green-700'>Adicionar</button>
+      <button type="submit" className="bg-green-400 text-white px-2 py-1 rounded-md hover:bg-green-700">
+        {card ? "Salvar Alterações" : "Adicionar Playlist"}
+      </button>
     </form>
   );
 };
