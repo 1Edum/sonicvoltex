a12d3d482820511fea1f49cf53c15fd30bcd2bb3
1Edum <eduardomatosdesouza2@gmail.com>
2024-09-11 19:17:57 -0300

    feat: updating with lucide icon

--- src/components/Play.tsx
+++ src/components/Play.tsx
@@ -1,22 +1,10 @@
 import React from "react";
+import { Play } from "lucide-react";
 
-export default function Play() {
+export default function PlayIcon() {
   return (
     <div className="p-2 rounded-full w-10 h-10 flex justify-center items-center bg-green-400">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        className="w-6 h-6"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
-        />
-      </svg>
+      <Play />      
     </div>
   );
 }
