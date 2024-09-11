c873212c01af6f7cd3d0a733745ae538102bcb8a
1Edum <eduardomatosdesouza2@gmail.com>
2024-09-11 19:18:45 -0300

    feat: add variants in card-root component

--- src/components/card/card-root.tsx
+++ src/components/card/card-root.tsx
@@ -1,16 +1,26 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode } from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
+import cn from 'classnames'; // Certifique-se de instalar a biblioteca `classnames` antes de usar
 
-interface CardRootProps {
-    children: ReactNode,
-    key?: number
+const CardRootVariants = cva(
+  'flex w-full rounded ',
+  {
+    variants: {
+      variant: {
+        default:'justify-between items-center hover:bg-zinc-300 rounded-md p-2 dark:hover:bg-zinc-800',
+        teste: 'flex-col-reverse justify-center group  hover:bg-white dark:hover:bg-zinc-800 object-cover p-4',
+      },
+    },
+  }
+);
+
+interface CardRootProps extends VariantProps<typeof CardRootVariants> {
+  children: ReactNode;
+  key?: number;
 }
 
-function CardRoot({children}: CardRootProps) {
-  return (
-    <div className='w-full flex justify-between items-center hover:bg-zinc-300 rounded-md p-2 dark:hover:bg-zinc-800'>
-        {children}
-    </div>
-  )
+function CardRoot({ children, variant }: CardRootProps) {
+  return <div className={cn(CardRootVariants({ variant }))}>{children}</div>;
 }
 
-export default CardRoot
+export default CardRoot;
