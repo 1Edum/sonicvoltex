41c16e5e06fce3b77ba1582dcc32449001be1a7c
1Edum <eduardomatosdesouza2@gmail.com>
2024-09-13 11:03:11 -0300

    feat: add favorites variant

--- src/components/card/card-root.tsx
+++ src/components/card/card-root.tsx
@@ -1,14 +1,15 @@
 import React, { ReactNode } from 'react';
 import { cva, type VariantProps } from 'class-variance-authority';
-import cn from 'classnames'; // Certifique-se de instalar a biblioteca `classnames` antes de usar
+import cn from 'classnames';
 
 const CardRootVariants = cva(
   'flex w-full rounded ',
   {
     variants: {
       variant: {
-        default:'justify-between items-center hover:bg-zinc-300 rounded-md p-2 dark:hover:bg-zinc-800',
-        teste: 'flex-col-reverse justify-center group  hover:bg-white dark:hover:bg-zinc-800 object-cover p-4',
+        default:'justify-between items-center hover:bg-white rounded-md p-2 group dark:hover:bg-zinc-800',
+        favorites: 'justify-between items-center hover:bg-white rounded-md  group dark:hover:bg-zinc-800 bg-zinc-200 dark:bg-zinc-700',
+        musics: 'flex-col-reverse justify-center group  hover:bg-white dark:hover:bg-zinc-800 object-cover p-4 ',
       },
     },
   }
