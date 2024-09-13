92753a2ee5c8e76e1c21ad528f1b887b19af18ac
1Edum <eduardomatosdesouza2@gmail.com>
2024-09-13 11:03:49 -0300

    feat: updating in styles

--- src/components/Main/Favorites.tsx
+++ src/components/Main/Favorites.tsx
@@ -1,6 +1,10 @@
 import React from "react";
-import { musicas } from "@/Dados/Musicas";
-import Play from "../Play";
+import { musicas } from "../../data/Musicas";
+import CardRoot from "../card/card-root";
+import Image from "next/image";
+import CardTitle from "../card/card-title";
+import CardDescription from "../card/card-description";
+import PlayIcon from "../play";
 
 export default function Favorites() {
   const idsSelecionados = [1, 7, 8, 4, 5, 9, 10, 11];
@@ -9,21 +13,52 @@ export default function Favorites() {
   );
 
   return (
-    <section className="flex flex-col overflow-x-auto">
-      <div className="grid grid-rows-2 grid-flow-col w-[800px] gap-y-1 md:gap-x-3 gap-x-1 md:w-full h-1/4 ">
-        {musicasSelecionadas.map((musica) => (
-          <div key={musica.id} className="grid grid-cols-4 grid-rows-1 bg-white bg-opacity-50 group rounded-sm hover:bg-opacity-100">
-            <img src={musica.img} alt="" className="row-span-2 w-full h-full"/>
-            <div className="col-span-2 w-full flex flex-col my-auto mx-2">
-              <h4 className="text-sm md:text-base">{musica.titulo}</h4>
-              <p className="text-xs">{musica.artista}</p>
-            </div>
-            <div className="row-span-2  hidden group-hover:flex justify-center items-center">
-              <Play />
-            </div>
+    <>
+    <h1 className="text-xl">Adicionado ao Favoritos</h1>
+    <section className="grid grid-rows-2 grid-flow-col gap-3 h-1/4 p-4">
+      {musicasSelecionadas.map((musica) => (
+        <CardRoot key={musica.id} variant={"favorites"}>
+          <div className="flex">
+          <Image
+            src={musica.img}
+            alt={musica.titulo}
+            className="w-20 h-20 rounded"
+            height={100}
+            width={100}
+          />
+          <div  className="flex flex-col justify-center pl-5">
+
+            <CardTitle title={musica.titulo}/>
+            <CardDescription description={musica.artista}/>
           </div>
-        ))}
-      </div>
-    </section>
+          </div>
+          
+            <div className="row-span-2 opacity-0 group-hover:opacity-100 justify-center items-center">
+                <PlayIcon />
+              </div>
+          
+        </CardRoot>
+      ))}
+      </section>
+    </>
   );
 }
+{/*
+  <section className="flex flex-col overflow-x-auto">
+        <div className="grid grid-rows-2 grid-flow-col w-[700px] gap-y-1 md:gap-x-3 gap-x-1 md:w-full h-1/4 ">
+          {musicasSelecionadas.map((musica) => (
+            <div
+              key={musica.id}
+              className="grid grid-cols-4 grid-rows-1 bg-white bg-opacity-50 group rounded-sm hover:bg-opacity-100"
+            >
+
+              <div className="row-span-2  hidden group-hover:flex justify-center items-center">
+                <Play />
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+  
+  
+  */}
