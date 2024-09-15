ae53a185499598002f18012aef85cd227cd05007
1Edum <eduardomatosdesouza2@gmail.com>
2024-09-15 19:18:10 -0300

    feat: add styles in footer component

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,7 +3,6 @@ import { musicas } from "../../data/Musicas";
 import Image from "next/image";
 import CardTitle from "../card/card-title";
 import CardDescription from "../card/card-description";
-import PlayIcon from "../play";
 import CardRoot from "../card/card-root";
 import {
   AudioLines,
@@ -23,10 +22,10 @@ function Footer() {
     idsSelecionados.includes(musica.id)
   );
   return (
-    <footer className="flex justify-between items-center w-full h-[10vh] px-4 dark:bg-zinc-700 bg-zinc-100">
+    <footer className="flex justify-between items-center w-full h-[10vh] px-4 dark:bg-zinc-950 bg-zinc-100">
       <div className="w-1/3 flex justify-center items-center">
         {musicasSelecionadas.map((musica) => (
-          <CardRoot key={musica.id} variant={"favorites"}>
+          <CardRoot key={musica.id} variant={"favorites"} className="dark:bg-zinc-950">
             <div className="flex">
               <Image
                 src={musica.img}
@@ -43,19 +42,19 @@ function Footer() {
           </CardRoot>
         ))}
       </div>
-      <div className="flex w-1/3 justify-center items-center">
+      <div className="flex w-1/3 justify-center items-center gap-x-3">
         <Shuffle className="icons"/>
         <SkipBack className="icons"/>
         <Pause className="icons"/>
         <SkipForward className="icons"/>
         <AudioLines className="icons"/>
       </div>
-      <div className="flex w-1/3 justify-end items-center">
+      <div className="flex w-1/3 justify-end items-center gap-x-3">
         <Speaker className="icons"/>
         <SquarePlay className="icons"/>
         <Volume1 className="icons"/>
-        <div className="w-16 h-2 rounded-md relative bg-black">
-            <div className="absolute w-3 h-2 rounded-md bg-zinc-400"/>
+        <div className="w-16 h-2 rounded-md relative dark:bg-zinc-900 bg-zinc-400">
+            <div className="absolute w-3 h-2 rounded-md bg-zinc-200"/>
         </div>
         <MonitorDown className="icons"/>
       </div>
