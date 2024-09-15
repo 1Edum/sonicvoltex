0be5ce8056c3c2586b7843b045d97a20f81c146b
1Edum <eduardomatosdesouza2@gmail.com>
2024-09-15 19:17:47 -0300

    feat: add theme-provader in page initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,26 @@
 import ListMusicsProps from "@/components/main/list-musics";
 import Favorites from "@/components/main/favorites";
 import SectionTop from "@/components/main/sectionTop";
-
 import { useState } from "react";
-
+import ThemeProvider from "@/components/ThemeContext";
 
 const Home = () => {
   const [state, setState] = useState('light');
   return (
-    <section className={"flex-1 md:w-4/5 md:h-[90vh] w-screen bg-gradient-to-t " + `${state === 'dark' ? 'from-zinc-600' : 'from-zinc-100'} from-70% to-green-400`}>
-      <section className="overflow-y-auto h-[90vh] flex flex-col px-3 z-0">
-      <SectionTop state={state} setState={setState} />
-        <Favorites />
+
+    <section className={"flex-1 md:w-4/5 md:h-[90vh] w-screen dark:bg-zinc-900 "}>
+      <section className="overflow-y-auto h-[90vh]  z-0">
+        <div className="flex flex-col bg-gradient-to-b from-green-500 dark:to-zinc-900 to-zinc-100">
+          <SectionTop state={state} setState={setState} />
+          <Favorites />
+        </div>
+        <ThemeProvider>
         <ListMusicsProps title="As melhores" />
         <ListMusicsProps title="As melhores" />
-        <p>{state}</p>
+        </ThemeProvider>
       </section>
     </section>
+
   );
 };
 export default Home;
