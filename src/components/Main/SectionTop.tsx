eddd2ee3b3c6d404df5a4c13b53549132edb0218
1Edum <eduardomatosdesouza2@gmail.com>
2024-09-15 14:38:29 -0300

    feat: update in section-top component

--- src/components/Main/SectionTop.tsx
+++ src/components/Main/SectionTop.tsx
@@ -1,26 +1,31 @@
-import { Bell, Settings, UserRound } from 'lucide-react';
-import React from 'react'
+import { Bell, Moon, Settings, Sun, UserRound } from "lucide-react";
+import React, { useContext } from "react";
+import { ThemeContext } from "../ThemeContext";
 
-interface SectionTopProps{
-    state : string
-    setState: (newState: string) => void;
+interface SectionTopProps {
+  state: string;
+  setState: (newState: string) => void;
 }
 
-export default function SectionTop( {state, setState }: SectionTopProps) {
+export default function SectionTop({ state, setState }: SectionTopProps) {
+  // Obtém o contexto do tema
+  const themeContext = useContext(ThemeContext);
+
+  // Verifica se o contexto existe antes de usar 'theme' e 'setTheme'
+  if (!themeContext) {
+    return <p>Contexto não encontrado!</p>; // Exibe uma mensagem de erro adequada
+  }
+
+  const { theme, setTheme } = themeContext;
+
   return (
-    <header className={"flex items-center justify-end gap-x-3 px-3 py-6 h-[84px] " }>
-        <div className='icons'>
-        <Settings />
-        </div>
-        <div className='icons'>
-            <Bell />
-        </div>
-        <div className='icons'>
-            <UserRound />
-        </div>
-            <button onClick={() => setState(state === 'dark' ? 'light' : 'dark')}>
-                Change State
-            </button>
+    <header className={`flex items-center justify-end gap-x-3 px-3 py-6 h-[84px] ${theme === 'dark' ? 'dark' : ''}`}>
+      <Settings className="icons" />
+      <Bell className="icons" />
+      <UserRound className="icons" />
+      <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
+         {theme === 'light' ? <Moon className="icons"/> : <Sun className="icons"/>}
+      </button>
     </header>
-  )
+  );
 }
